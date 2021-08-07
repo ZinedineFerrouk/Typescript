2ed7452def70b849160f17fe17fc2d31399a7ecb
Zinedine <zinedine27000@gmail.com>
2021-08-07 23:55:32 +0200

    Mixed types in typescript

--- sandbox.ts
+++ sandbox.ts
@@ -26,12 +26,12 @@ console.log(circ(15));
 
 // Si on déclare un tableau avec un seul type de data il n'acceptera que ce type
 let names = ['luigi', 'mario', 'yoshi']
-names.push('toad')
-names.push(15)
+names.push('toad') 
+// names.push(15) --> renvoi une erreur
 
 
 let numbers = [13, 27, 54]
-numbers.push('toad')
+// numbers.push('toad') --> renvoi une erreur
 numbers.push(15)
 
 // Objects
@@ -39,4 +39,45 @@ let ninja = {
     name: 'mario',
     belt: 'black',
     age: 45
+}
+
+// Explicit types
+let personnage: string;
+let vie: number;
+let isDead: boolean;
+
+// personnage = 26 --> renvoi une erreur
+personnage = 'Goku'
+
+// Arrays
+// let ninjas: string[] /* Déclare que la variable ninjas est un tableau de string */
+let ninjas: string[] = [] 
+ninjas.push('shaun');
+
+// Union types
+// On déclare une variable de type Array qui accepte les string, number et boolean, 
+// On l'initialise à un tableau vide afin de pouvoir push() de la data  
+let mixedArray: (string|number|boolean)[] = []
+mixedArray.push('hello')
+mixedArray.push(27)
+mixedArray.push(false)
+
+let uid: string|number;
+uid = 'accepte les string et les number'
+uid = 27
+
+console.log(mixedArray);
+
+// Objects
+let obj: object;
+let obj2: {};
+
+obj = {
+    name: 'Zinedine',
+    age: 23
+}
+
+obj2 = {
+    name:'Amana',
+    nom: 'Koyim'
 }
